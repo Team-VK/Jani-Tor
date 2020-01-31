4abc51c704b70f1a313b568549eb35832f260295
Tomi Koskipahta <tomi@koskipahta.fi>
2020-01-31 23:56:47 +0200

    More player movement, with camera follow

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -6,7 +6,8 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody rigid;
-    public float speed = 6.0f;
+    public float speed = 20.0f;
+    public float rotateSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,67 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-        Debug.Log("############# " + x + " " + y);
-        Vector3 move = new Vector3(x, 0f, y);
-        rigid.AddForce(move * speed);
+        maybeQuit();
+        Vector3 movementVector = getMovementVector();
+        Debug.Log(movementVector);
+        handleMovemenent(movementVector);
+        //handleRotation(movementVector);
+
+    }
+
+    void handleMovemenent(Vector3 movement) 
+    {
+        var delta = Time.deltaTime;
+        Debug.Log("FORCE: " + movement);
+        rigid.AddForce(movement);
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    void handleRotation(Vector3 movement) 
+    {
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(movement.x, 0, movement.z);
+
+        var rotate = Time.deltaTime * rotateSpeed;
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, rotate);
+    }
+
+    Vector3 getMovementVector() 
+    {
+        var delta = Time.deltaTime;
+        var x = 0f;
+        var y = 0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            x += transform.position.x - (speed * delta);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x += transform.position.x + (speed * delta);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            y += transform.position.y + (speed * delta);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            y += transform.position.y - (speed * delta);
+        }
+
+        Debug.Log("" 
+            + Input.GetKey(KeyCode.LeftArrow) + "-" 
+            + Input.GetKey(KeyCode.RightArrow) + "-"
+            + Input.GetKey(KeyCode.UpArrow) + "-"
+            + Input.GetKey(KeyCode.DownArrow));
+
+        return new Vector3(x, 0f, y);
+    }
+
+    void maybeQuit() 
+    {
+        if (Input.GetKey ("escape")) {
+            Application.Quit();
+        }
     }
 }
