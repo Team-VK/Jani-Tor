f8fcadb9272f132e9b074bdbad939349b9ea3b03
Mika Aho <mika.aho@eficode.com>
2020-02-01 15:48:40 +0200

    Tool bound to hand bone

--- Assets/Code/ToolBehavior.cs
+++ Assets/Code/ToolBehavior.cs
@@ -7,7 +7,7 @@ public class ToolBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+        //this.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
