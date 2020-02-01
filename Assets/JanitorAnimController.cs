ee230b06468d2df10126a8b4520c576f34a71a1a
Mika Aho <mika.aho@eficode.com>
2020-02-01 13:05:05 +0200

    Animations bound to janitor!

---
+++ Assets/JanitorAnimController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanitorAnimController : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            animator.Play("Walk");
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            animator.Play("Swing");
+        }
+
+        if (!Input.GetKey("up") && !Input.GetKey("space"))
+        {
+            animator.Play("Idle");
+        }
+        
+    }
+}
