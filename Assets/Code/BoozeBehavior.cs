a45a297fae834fea8bb5cd351375303335d6b7aa
Mika Aho <mika.aho@eficode.com>
2020-02-02 00:23:52 +0200

    Developing booze mechanic

---
+++ Assets/Code/BoozeBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoozeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Vector3> spawnpoints;
+
+    void Start()
+    {
+        Vector3 spawnpoint1 = new Vector3(1, 2, 3);
+        Vector3 spawnpoint2 = new Vector3(1, 2, 3);
+        spawnpoints = new List<Vector3>();
+        spawnpoints.Add(spawnpoint1);
+        spawnpoints.Add(spawnpoint2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnBooze()
+    {
+        int random_spawnpoint = Random.Range(0, spawnpoints.Count-1);
+
+    }
+}
